a72302c3eaef4ad463068eb7c48110a0c70b9878
ltchuan <ltchuan@gmail.com>
2017-03-10 15:10:18 +1300

    Forgot to save index.js and add the new folder

--- Module_2/index.js
+++ Module_2/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.logger('dev'));
-app.use(express.favicon());
-app.use(express.static(__dirname + '/public'));
+app.configure( function () {
+	
+	app.use(express.logger('dev'));
+	app.use(express.favicon());
+	app.use(express.static(__dirname + '/public'));
+});
 
 app.listen(3000);
